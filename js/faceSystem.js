3353c81855fdeca399fe9bf88f14b57a89afd51c
Attendance-Portal <122359623+Attendance-Portal@users.noreply.github.com>
2023-01-11 13:37:23 +0530

    Update faceSystem.js

--- js/faceSystem.js
+++ js/faceSystem.js
@@ -22,7 +22,8 @@ $(document).ready(function(){
 //,
 // , 'Sejal', 'Saumya', 'Shrivats', 'Suparna', 'Suchita', 'Srajan', 'Suchita', , 'Suparna','YuvrajJagdhane''Vishvender', 'Vivek','Tejavath,'Ananya' , 'AshutoshSingh' ,'Kaushik.jpg', 'Kushagra.jpg','AyushDubey' ,'BaniSingh','Banoth', 'Harshit', 'Kaushik', 'gautam']
 
-        const labels = ['monika','khushboo','Kareena', 'Aarya', 'AaryaSuhas', 'Abhinav', 'AbhishekKumarSingh','Adarsh', 'Aditi', 'Advait', 'Amit','Aniket','AnkitKumar', 'Aruprakash','Aryan', 'AryanGupta', 'AryanSrivastava', 'Aryman', 'Bharat', 'chandler', 'Chandu', 'Dev','Divyanth','Lisha', 'Mudavath', 'Nikhil', 'Nunavath','Priyansh', 'Rachaprolu', 'Rajdeep', 'Rajitha', 'RajPrakash', 'Ritika','Rupsona', 'Samridhdi', 'Sandipam', 'Sanskar', 'Sarthak', 'Sejal', 'ShantanuSingh', 'Shivam', 'Shweta','Srajan', 'Vansh','Vikas','Vinod', 'Vishal']
+        const labels = ['monika','khushboo']
+//         ,'Kareena', 'Aarya', 'AaryaSuhas', 'Abhinav', 'AbhishekKumarSingh','Adarsh', 'Aditi', 'Advait', 'Amit','Aniket','AnkitKumar', 'Aruprakash','Aryan', 'AryanGupta', 'AryanSrivastava', 'Aryman', 'Bharat', 'chandler', 'Chandu', 'Dev','Divyanth','Lisha', 'Mudavath', 'Nikhil', 'Nunavath','Priyansh', 'Rachaprolu', 'Rajdeep', 'Rajitha', 'RajPrakash', 'Ritika','Rupsona', 'Samridhdi', 'Sandipam', 'Sanskar', 'Sarthak', 'Sejal', 'ShantanuSingh', 'Shivam', 'Shweta','Srajan', 'Vansh','Vikas','Vinod', 'Vishal']
 
         const labeledFaceDescriptors = await Promise.all(
             labels.map(async label => {
