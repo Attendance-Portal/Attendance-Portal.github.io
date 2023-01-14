6fbe5dd31a996642ebbeaa6661d68791448b9bfb
Attendance-Portal <122359623+Attendance-Portal@users.noreply.github.com>
2023-01-14 20:19:57 +0530

    Update faceSystem.js

--- js/faceSystem.js
+++ js/faceSystem.js
@@ -20,15 +20,13 @@ $(document).ready(function(){
 //         faceapi.draw.drawFaceExpressions(canvas, faceDescriptions)
 
         
-        //         const labels = ['ross', 'rachel', 'chandler', 'monica', 'phoebe', 'joey', , 'Sejal','ShantanuSingh', 'Shivam', 'Shweta','Srajan',, 'Vansh','Vikas','Vinod', 'Vishal']
-        
+//         const labels = ['ross', 'rachel', 'chandler', 'monica', 'phoebe', 'joey']
         const labels = ['monika','khushboo']
-//         ,'Kareena', 'Aarya', 'AaryaSuhas', 'Abhinav', 'AbhishekKumarSingh','Adarsh', 'Aditi', 'Advait', 'Amit', 'Aniket','AnkitKumar', 'Aruprakash','Aryan', 'AryanGupta', 'AryanSrivastava', 'Aryman', 'Bharat', 'chandler', 'Chandu',  'Dev','Divyanth','Lisha', 'Mudavath', 'Nikhil', 'Nunavath','Priyansh', 'Rachaprolu', 'Rajdeep', 'Rajitha', 'RajPrakash','Ritika','Rupsona', 'Samridhdi', 'Sandipam', 'Sanskar', 'Sarthak']
 
         const labeledFaceDescriptors = await Promise.all(
             labels.map(async label => {
 
-                const imgUrl = `images/${label}.jpg`
+                const imgUrl = 'images/${label}.jpg`
                 const img = await faceapi.fetchImage(imgUrl)
                 
                 const faceDescription = await faceapi.detectSingleFace(img)
