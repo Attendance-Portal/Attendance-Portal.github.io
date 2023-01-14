9e515cbb00c5b785ebdb0ec1a9f2c2daa37344a9
Attendance-Portal <122359623+Attendance-Portal@users.noreply.github.com>
2023-01-15 00:16:22 +0530

    Update faceSystem.js

--- js/faceSystem.js
+++ js/faceSystem.js
@@ -20,7 +20,8 @@ $(document).ready(function(){
         //faceapi.draw.drawFaceExpressions(canvas, faceDescriptions)
 
        
-        const labels = ['monika','khushboo','Kareena', 'Aarya', 'AaryaSuhas', 'Abhinav', 'AbhishekKumarSingh','Adarsh', 'Aditi', 'Advait', 'Amit', 'Aniket','AnkitKumar', 'Aruprakash','Aryan', 'AryanGupta', 'AryanSrivastava', 'Aryman', 'Bharat', 'chandler', 'Chandu',  'Dev','Divyanth','Lisha', 'Mudavath', 'Nikhil', 'Nunavath','Priyansh', 'Rachaprolu', 'Rajdeep', 'Rajitha', 'RajPrakash','Ritika','Rupsona', 'Samridhdi', 'Sandipam', 'Sanskar', 'Sarthak']
+        const labels = ['monika','khushboo']
+        //,'Kareena', 'Aarya', 'AaryaSuhas', 'Abhinav', 'AbhishekKumarSingh','Adarsh', 'Aditi', 'Advait', 'Amit', 'Aniket','AnkitKumar', 'Aruprakash','Aryan', 'AryanGupta', 'AryanSrivastava', 'Aryman', 'Bharat', 'chandler', 'Chandu',  'Dev','Divyanth','Lisha', 'Mudavath', 'Nikhil', 'Nunavath','Priyansh', 'Rachaprolu', 'Rajdeep', 'Rajitha', 'RajPrakash','Ritika','Rupsona', 'Samridhdi', 'Sandipam', 'Sanskar', 'Sarthak']
 
         const labeledFaceDescriptors = await Promise.all(
             labels.map(async label => {
@@ -48,6 +49,7 @@ $(document).ready(function(){
         results.forEach((bestMatch, i) => {
             const box = faceDescriptions[i].detection.box
             const text = bestMatch.toString()
+            document.write(text)
             const drawBox = new faceapi.draw.DrawBox(box, { label: text })
             drawBox.draw(canvas)
         })
