b0badee59272b527f2703260211f8f1eac20fbb5
Attendance-Portal <122359623+Attendance-Portal@users.noreply.github.com>
2023-01-14 18:43:52 +0530

    Update faceSystem.js

--- js/faceSystem.js
+++ js/faceSystem.js
@@ -16,13 +16,16 @@ $(document).ready(function(){
 
         faceDescriptions = faceapi.resizeResults(faceDescriptions, img)
         faceapi.draw.drawDetections(canvas, faceDescriptions)
-        faceapi.draw.drawFaceLandmarks(canvas, faceDescriptions)
-        faceapi.draw.drawFaceExpressions(canvas, faceDescriptions)
+//         faceapi.draw.drawFaceLandmarks(canvas, faceDescriptions)
+//         faceapi.draw.drawFaceExpressions(canvas, faceDescriptions)
 
         
-//         const labels = ['ross', 'rachel', 'chandler', 'monica', 'phoebe', 'joey']
+//         const labels = ['ross', 'rachel', 'chandler', 'monica', 'phoebe', 'joey', , 'Sejal','ShantanuSingh', 'Shivam', 'Shweta','Srajan',, 'Vansh','Vikas','Vinod', 'Vishal']
         
-        const labels = ['monika','khushboo','Kareena', 'Aarya', 'AaryaSuhas', 'Abhinav', 'AbhishekKumarSingh','Adarsh', 'Aditi', 'Advait', 'Amit','Aniket','AnkitKumar', 'Aruprakash','Aryan', 'AryanGupta', 'AryanSrivastava', 'Aryman', 'Bharat', 'chandler', 'Chandu', 'Dev','Divyanth','Lisha', 'Mudavath', 'Nikhil', 'Nunavath','Priyansh', 'Rachaprolu', 'Rajdeep', 'Rajitha', 'RajPrakash', 'Ritika','Rupsona', 'Samridhdi', 'Sandipam', 'Sanskar', 'Sarthak', 'Sejal', 'ShantanuSingh', 'Shivam', 'Shweta','Srajan', 'Vansh','Vikas','Vinod', 'Vishal']
+        const labels = ['monika','khushboo','Kareena', 'Aarya', 'AaryaSuhas', 'Abhinav', 'AbhishekKumarSingh','Adarsh', 'Aditi', 'Advait', 'Amit',
+                        'Aniket','AnkitKumar', 'Aruprakash','Aryan', 'AryanGupta', 'AryanSrivastava', 'Aryman', 'Bharat', 'chandler', 'Chandu', 
+                        'Dev','Divyanth','Lisha', 'Mudavath', 'Nikhil', 'Nunavath','Priyansh', 'Rachaprolu', 'Rajdeep', 'Rajitha', 'RajPrakash', 
+                        'Ritika','Rupsona', 'Samridhdi', 'Sandipam', 'Sanskar', 'Sarthak']
 
         const labeledFaceDescriptors = await Promise.all(
             labels.map(async label => {
