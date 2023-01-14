a6c04036c912c0c6545b30c86236ad8db4df0e7e
Attendance-Portal <122359623+Attendance-Portal@users.noreply.github.com>
2023-01-14 18:11:30 +0530

    Update faceSystem.js

--- js/faceSystem.js
+++ js/faceSystem.js
@@ -16,26 +16,26 @@ $(document).ready(function(){
 
         faceDescriptions = faceapi.resizeResults(faceDescriptions, img)
         faceapi.draw.drawDetections(canvas, faceDescriptions)
-//        faceapi.draw.drawFaceLandmarks(canvas, faceDescriptions)
-//        faceapi.draw.drawFaceExpressions(canvas, faceDescriptions)
+        faceapi.draw.drawFaceLandmarks(canvas, faceDescriptions)
+        faceapi.draw.drawFaceExpressions(canvas, faceDescriptions)
 
-// , 'Sejal', 'Saumya', 'Shrivats', 'Suparna', 'Suchita', 'Srajan', 'Suchita', , 'Suparna','YuvrajJagdhane''Vishvender', 'Vivek','Tejavath,'Ananya' , 'AshutoshSingh' ,'Kaushik.jpg', 'Kushagra.jpg','AyushDubey' ,'BaniSingh','Banoth', 'Harshit', 'Kaushik', 'gautam']
-
+        
+//         const labels = ['ross', 'rachel', 'chandler', 'monica', 'phoebe', 'joey']
+        
         const labels = ['monika','khushboo']
-        //,'Kareena', 'Aarya', 'AaryaSuhas', 'Abhinav', 'AbhishekKumarSingh','Adarsh', 'Aditi', 'Advait', 'Amit','Aniket','AnkitKumar', 'Aruprakash','Aryan', 'AryanGupta', 'AryanSrivastava', 'Aryman', 'Bharat', 'chandler', 'Chandu', 'Dev','Divyanth','Lisha', 'Mudavath', 'Nikhil', 'Nunavath','Priyansh', 'Rachaprolu', 'Rajdeep', 'Rajitha', 'RajPrakash', 'Ritika','Rupsona', 'Samridhdi', 'Sandipam', 'Sanskar', 'Sarthak', 'Sejal', 'ShantanuSingh', 'Shivam', 'Shweta','Srajan', 'Vansh','Vikas','Vinod', 'Vishal']
 
         const labeledFaceDescriptors = await Promise.all(
             labels.map(async label => {
 
-                const imgUrl = 'images/${label}.jpg'
+                const imgUrl = `images/${label}.jpg`
                 const img = await faceapi.fetchImage(imgUrl)
-
+                
                 const faceDescription = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
-
+                
                 if (!faceDescription) {
                 throw new Error(`no faces detected for ${label}`)
                 }
-                document.write("label: "+label)
+                
                 const faceDescriptors = [faceDescription.descriptor]
                 return new faceapi.LabeledFaceDescriptors(label, faceDescriptors)
             })
@@ -49,7 +49,6 @@ $(document).ready(function(){
         results.forEach((bestMatch, i) => {
             const box = faceDescriptions[i].detection.box
             const text = bestMatch.toString()
-            document.write("text: "+text)
             const drawBox = new faceapi.draw.DrawBox(box, { label: text })
             drawBox.draw(canvas)
         })
