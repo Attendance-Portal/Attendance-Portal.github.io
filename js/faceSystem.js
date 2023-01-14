206512660939ddb3cdc950c242ad49c022c5fb54
Attendance-Portal <122359623+Attendance-Portal@users.noreply.github.com>
2023-01-14 19:49:53 +0530

    Update faceSystem.js

--- js/faceSystem.js
+++ js/faceSystem.js
@@ -16,11 +16,12 @@ $(document).ready(function(){
 
         faceDescriptions = faceapi.resizeResults(faceDescriptions, img)
         faceapi.draw.drawDetections(canvas, faceDescriptions)
-        faceapi.draw.drawFaceLandmarks(canvas, faceDescriptions)
-        faceapi.draw.drawFaceExpressions(canvas, faceDescriptions)
+//         faceapi.draw.drawFaceLandmarks(canvas, faceDescriptions)
+//         faceapi.draw.drawFaceExpressions(canvas, faceDescriptions)
 
         
-        const labels = ['monika','khushboo','Kareena', 'Aarya', 'AaryaSuhas', 'Abhinav', 'AbhishekKumarSingh','Adarsh', 'Aditi', 'Advait', 'Amit', 'Aniket','AnkitKumar', 'Aruprakash','Aryan', 'AryanGupta', 'AryanSrivastava', 'Aryman', 'Bharat', 'chandler', 'Chandu',  'Dev','Divyanth','Lisha', 'Mudavath', 'Nikhil', 'Nunavath','Priyansh', 'Rachaprolu', 'Rajdeep', 'Rajitha', 'RajPrakash','Ritika','Rupsona', 'Samridhdi', 'Sandipam', 'Sanskar', 'Sarthak']
+        const labels = ['monika','khushboo']
+//         ,'Kareena', 'Aarya', 'AaryaSuhas', 'Abhinav', 'AbhishekKumarSingh','Adarsh', 'Aditi', 'Advait', 'Amit', 'Aniket','AnkitKumar', 'Aruprakash','Aryan', 'AryanGupta', 'AryanSrivastava', 'Aryman', 'Bharat', 'chandler', 'Chandu',  'Dev','Divyanth','Lisha', 'Mudavath', 'Nikhil', 'Nunavath','Priyansh', 'Rachaprolu', 'Rajdeep', 'Rajitha', 'RajPrakash','Ritika','Rupsona', 'Samridhdi', 'Sandipam', 'Sanskar', 'Sarthak']
 
         const labeledFaceDescriptors = await Promise.all(
             labels.map(async label => {
@@ -28,7 +29,8 @@ $(document).ready(function(){
                 const imgUrl = `images/${label}.jpg`
                 const img = await faceapi.fetchImage(imgUrl)
                 
-                const faceDescription = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+                const faceDescription = await faceapi.detectSingleFace(img)
+//                 .withFaceLandmarks().withFaceDescriptor()
                 
                 if (!faceDescription) {
                 throw new Error(`no faces detected for ${label}`)
